5356926473d3b3781a378229b11a688a49ee9018
hln5 <harrynguyen019@gmail.com>
2023-05-01 11:17:42 -0700

    separate some logic into model class

--- Model/Network.py
+++ Model/Network.py
@@ -1,14 +1,36 @@
+from Model.Node import Node 
+from Model.Layer import Layer
 
 class Network():
     def __init__(self):
-        self.nodes = []
         self.layers = []
-    
+        self.edges = []
+
     def add_edges(self):
-        pass
+        numLayers = len(self.layers)
+        for i in range(numLayers):
+            isLastLayer = (i == (numLayers - 1))
+            if isLastLayer:
+                break
+
+            self._add_edges_between_layers(i, i + 1)
     
-    def add_layer(self, size):
-        pass
+    def _add_edges_between_layers(self, i, j):
+        assert i < j, "layer 1 must be less than layer 2"
+        assert i >= 0, "first layer must be greater than -1"
+        assert j < len(self.layers), "second layer cannot be greater than number of total layers"
+
+        layer1 = self.layers[i]
+        layer2 = self.layers[j]
+        for node1 in layer1.nodes:
+            for node2 in layer2.nodes:
+                self.edges.append((node1, node2))
+    
+    def add_layer(self, size, color):
+        newNodes = [Node(color) for _ in range(size)]
+        newLayer = Layer(newNodes)
+        self.layers.append(newLayer)
+        self.add_edges()
     
     def remove_layer(self):
         pass
@@ -16,7 +38,7 @@ class Network():
     def get_node_positions(self):
         nodePos = dict()
         for layerNum, layer in enumerate(self.layers):
-            nodePos.update((node, (layerNum, nodeNum)) for nodeNum, node in enumerate(layer))
+            nodePos.update((node, (layerNum, nodeNum)) for nodeNum, node in enumerate(layer.nodes))
         
         return nodePos
     
--- Widgets/neuralNet.py
+++ Widgets/neuralNet.py
@@ -1,6 +1,7 @@
-import uuid
 import networkx as nx
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+from Model.Network import Network
 
 # constants
 NODE_COLOR = 'black'
@@ -11,7 +12,7 @@ class NeuralNetwork(FigureCanvas):
 
         self.graph = graph
         self.figure = figure
-        self.layers = []
+        self.network = Network()
 
         self._draw_network()
 
@@ -20,7 +21,7 @@ class NeuralNetwork(FigureCanvas):
         self._add_nodes()
         self._add_edges()
 
-        node_positions = self._get_node_positions()
+        node_positions = self.network.get_node_positions()
         color_map = self._get_node_color_map(len(node_positions))
         nx.draw(self.graph, node_color=color_map, pos=node_positions, with_labels=False)
         self.draw_idle()
@@ -29,52 +30,24 @@ class NeuralNetwork(FigureCanvas):
         self.figure.clf()
     
     def add_layer(self, size):
-        newLayer = [self._generate_node_id() for _ in range(size)]
-        self.layers.append(newLayer)
+        self.network.add_layer(size, NODE_COLOR)
         self._draw_network()
     
     def remove_layer(self, i):
         pass
 
-    def _generate_node_id(self):
-        return str(uuid.uuid4())
-    
     def _get_node_color_map(self, num_nodes):
         color_map = []
         for _ in range(num_nodes):
             color_map.append(NODE_COLOR)
         
         return color_map
-        
-
-    def _get_node_positions(self):
-        nodePos = dict()
-        for layerNum, layer in enumerate(self.layers):
-            nodePos.update((node, (layerNum, nodeNum)) for nodeNum, node in enumerate(layer))
-        
-        return nodePos
     
     def _add_nodes(self):
-        for i, layer in enumerate(self.layers):
-            self.graph.add_nodes_from(layer, bipartite=i)
+        for i, layer in enumerate(self.network.layers):
+            self.graph.add_nodes_from(layer.nodes, bipartite=i)
 
     def _add_edges(self):
-        numLayers = len(self.layers)
-        for i in range(numLayers):
-            isLastLayer = (i == (numLayers - 1))
-            if isLastLayer:
-                break
-
-            self._add_edges_between_layers(i, i + 1)
-    
-    def _add_edges_between_layers(self, i, j):
-        assert i < j, "layer 1 must be less than layer 2"
-        assert i >= 0, "first layer must be greater than -1"
-        assert j < len(self.layers), "second layer cannot be greater than number of total layers"
-
-        layer1 = self.layers[i]
-        layer2 = self.layers[j]
-        for node1 in layer1:
-            for node2 in layer2:
-                self.graph.add_edge(node1, node2)
+        for node1, node2 in self.network.edges:
+            self.graph.add_edge(node1, node2)
             
